3bebfa11ab5f4749e648b83321b777d04d7b74aa
jwal1024 <67374091+jwal1024@users.noreply.github.com>
2026-05-26 11:24:43 +0100

    Update to meta-data retreival

--- survey_config.js
+++ survey_config.js
@@ -1,6 +1,6 @@
 window.SURVEY_CONFIG = {
     googleSheets: {
         enabled: true,
-        appUrl: "https://script.google.com/macros/s/AKfycbxJ9Qca0zFg7reamg4zSR5yqrjMNMO1KkjNBgaZeT62v69BI0RPPA8MJjqztRnDrCJs/exec"
+        appUrl: "https://script.google.com/macros/s/AKfycbwIMvz4luMurw04MpNZ8u-PsX6i96kBD5vgYwuTAm5CdUSntfjPXUtK1epz__89rNeU/exec"
     }
 };
