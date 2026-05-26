0d9f0a0c26165af1fad4ce9cfc575a2818aa6101
jwal1024 <67374091+jwal1024@users.noreply.github.com>
2026-05-26 08:49:13 +0100

    Add App URL

--- survey_config.js
+++ survey_config.js
@@ -1,6 +1,6 @@
 window.SURVEY_CONFIG = {
     googleSheets: {
         enabled: false,
-        appUrl: ""
+        appUrl: "https://script.google.com/macros/s/AKfycbxJ9Qca0zFg7reamg4zSR5yqrjMNMO1KkjNBgaZeT62v69BI0RPPA8MJjqztRnDrCJs/exec"
     }
 };
