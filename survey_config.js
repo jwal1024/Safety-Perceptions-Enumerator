4efaf9276fa7b18265656ed67a80498c9182c333
jwal1024 <67374091+jwal1024@users.noreply.github.com>
2026-05-26 09:57:50 +0100

    Live google sheets integration setup

--- survey_config.js
+++ survey_config.js
@@ -1,6 +1,6 @@
 window.SURVEY_CONFIG = {
     googleSheets: {
-        enabled: false,
+        enabled: true,
         appUrl: "https://script.google.com/macros/s/AKfycbxJ9Qca0zFg7reamg4zSR5yqrjMNMO1KkjNBgaZeT62v69BI0RPPA8MJjqztRnDrCJs/exec"
     }
 };
